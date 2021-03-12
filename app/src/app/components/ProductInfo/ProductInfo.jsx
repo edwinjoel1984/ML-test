c9b216d1fc48a7d8e057bd65cac470b7cdd28dee
Edwin Ulloa <edwinjoel1984@gmail.com>
2021-03-12 12:13:58 -0500

    implement carousel for product images

--- app/src/app/components/ProductInfo/ProductInfo.jsx
+++ app/src/app/components/ProductInfo/ProductInfo.jsx
@@ -1,6 +1,11 @@
+import Carousel from "nuka-carousel";
 import { formatPrice } from '../../utils'
 import './ProductInfo.scss'
 const ProductInfo = ({productInformation}) => {
+    const slickSettings = {
+        autoplay: true,
+        withoutControls: true
+      };
     const getStatusProduct = () =>{
         const statusProduct = productInformation.attributes.find(item=>item.id==="ITEM_CONDITION")
         if(statusProduct)
@@ -12,7 +17,13 @@ const ProductInfo = ({productInformation}) => {
         {productInformation.id && (
             <>
             <div className="general-information">
-                <img src={productInformation.pictures[0]?.secure_url} alt={productInformation.title} className="general-information__image" />
+                <div className="general-information__image">
+                    <Carousel {...slickSettings}>
+                        {productInformation.pictures.map(img=>{
+                            return <div key={img.id}><img src={img.secure_url} alt={productInformation.title} className="general-information__image" /></div>
+                        })}
+                    </Carousel>
+                </div>
                 <div>
                 <h2 className="general-information__headline">Descripción del Producto</h2>
                 <p className="general-information__description">{productInformation.description}</p>
@@ -21,7 +32,13 @@ const ProductInfo = ({productInformation}) => {
             <div className="price-information">
                 <p className="price-information__status-sold">{`${getStatusProduct()}  ${productInformation.sold_quantity} Vendidos`}</p>
                 <h2 className="price-information__title">{productInformation.title}</h2>
-                <img className="price-information__img-no-desktop" src={productInformation.pictures[0]?.secure_url} alt={productInformation.title} />
+                <div className="price-information__img-no-desktop">
+                    <Carousel {...slickSettings}>
+                        {productInformation.pictures.map(img=>{
+                            return <div key={img.id}><img src={img.secure_url} alt={productInformation.title}  /></div>
+                        })}
+                    </Carousel>
+                </div>
                 <p className="price-information__value">{formatPrice(productInformation.currency_id, productInformation.price)}</p>
                 <button className="price-information__btn-buy">Comprar</button>
             </div>
